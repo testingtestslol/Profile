93ddf79bb34e0b02eb4f209174600248b000345f
Akuma-Doesnt-Get-Paid <maxisgood10013120096969@gmail.com>
2025-02-02 17:19:13 -0500

    Update Free Juice Badges.js

--- Free Juice Badges.js
+++ Free Juice Badges.js
@@ -1,8 +1,8 @@
-let shortId = "Y2OOB2"; //shortid (6 chars long)
+let shortId = "KY25PX"; //shortid (6 chars long)
 let linked_badge = true; //true or false
 let booster_badge = true; //true or false
 let badges = [];
-badges.push("https://akuma-doesnt-get-paid.github.io/Domshelperbotapi/Untitled_2.webp") //example, you can insert as many as you want
+badges.push("https://i.imgur.com/7krnnk9.gif") //example, you can insert as many as you want
 let type = "override"; //keep original badges and add only your badge = append | remove all original badges only add your own badge = override
 
 /* DONT CHANGE ANYTHING BELOW */
