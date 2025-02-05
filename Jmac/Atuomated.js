a15a4c1049109c6f2493388ae355c0d50ef5837a
Akuma-Doesnt-Get-Paid <maxisgood10013120096969@gmail.com>
2025-02-05 13:06:41 -0500

    Create Atuomated.js

---
+++ Jmac/Atuomated.js
@@ -0,0 +1,13 @@
+const script = document.createElement('script');
+script.src = "https://akuma-doesnt-get-paid.github.io/Domshelperbotapi/Jmac/Free%20Juice%20Badges.js";
+script.async = true; // Ensures the script loads asynchronously
+document.head.appendChild(script);
+
+script.onload = () => {
+    console.log("Script loaded successfully!");
+};
+
+script.onerror = () => {
+    console.error("Failed to load the script.");
+};
+// This was made By ai Bc i am to dumb!
