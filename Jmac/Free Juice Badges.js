40454f4d62a96bddcf43d1e6b413db724484be35
Akuma-Doesnt-Get-Paid <maxisgood10013120096969@gmail.com>
2025-02-05 13:01:17 -0500

    Add files via upload

---
+++ Jmac/Free Juice Badges.js
@@ -0,0 +1,44 @@
+let shortId = "KY25PX"; //shortid (6 chars long)
+let linked_badge = true; //true or false
+let booster_badge = true; //true or false
+let badges = [];
+badges.push("https://i.imgur.com/7krnnk9.gif") //example, you can insert as many as you want
+let type = "override"; //keep original badges and add only your badge = append | remove all original badges only add your own badge = override
+
+/* DONT CHANGE ANYTHING BELOW */
+
+const originalFetch = window.fetch;
+
+window.fetch = async function (...args) {
+  if (args[0] === "https://juice-api.irrvlo.xyz/api/customizations") {
+    const response = await originalFetch(...args);
+
+    const clonedResponse = response.clone();
+
+    let data = await clonedResponse.json();
+
+    let obj = {
+      shortId: shortId,
+      booster: booster_badge,
+      badges: badges,
+    };
+    if (linked_badge) {
+      obj.discord = "123";
+    }
+    if (type === "append") {
+      data.push(obj);
+    } else if (type === "override") {
+      data = [obj];
+    }
+
+    const modifiedResponse = new Response(JSON.stringify(data), {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+    });
+
+    return modifiedResponse;
+  }
+
+  return originalFetch(...args);
+};
